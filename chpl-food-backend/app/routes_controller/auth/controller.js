d8d56be5ca2a8fa1c38ca55d0b587e12113a7a7d
Ridham44 <ridhampatel2004.44@gmail.com>
2025-05-29 09:52:56 +0530

    Enum

--- chpl-food-backend/app/routes_controller/auth/controller.js
+++ chpl-food-backend/app/routes_controller/auth/controller.js
@@ -3,11 +3,12 @@ const bcrypt = require('bcryptjs');
 const db = require('../../db/models');
 const { status } = require('../../../utils');
 const { Op } = require('sequelize');
-const { Enums } = require('../../../utils/lib/enums');
+const Enums = require('../../../utils/lib/enums');
 
 exports.login = async (req, res) => {
     try {
         const { identifier, password } = req.body;
+        const activeStatus = Enums.Status.get('Active').value;
 
         if (!identifier || !password) {
             return res.status(status.BadRequest).json({ message: 'Email or mobile number and password are required.' });
@@ -16,21 +17,21 @@ exports.login = async (req, res) => {
         const user = await db.User.scope(['withPassword', 'withoutTenant']).findOne({
             where: {
                 [Op.or]: [{ email: identifier }, { mobile: identifier }],
-                status: Enums.Status.get('Active').value,
+                status: activeStatus,
             },
             include: [
                 {
                     model: db.Role,
                     as: 'Role',
                     required: false,
-                    where: { status: Enums.Status.get('Active').value },
+                    where: { status: activeStatus },
                     disableTenantCheck: true,
                 },
                 {
                     model: db.Tenant,
                     as: 'Tenant',
                     required: false,
-                    where: { status: Enums.Status.get('Active').value },
+                    where: { status: activeStatus },
                     disableTenantCheck: true,
                 },
             ],
