b74e41396f6065cdfe711933aea74cb8538f5c42
Ridham44 <ridhampatel2004.44@gmail.com>
2025-05-29 16:38:16 +0530

    index operatorsAliases: flase to 0

--- chpl-food-backend/app/db/models/index.js
+++ chpl-food-backend/app/db/models/index.js
@@ -15,7 +15,7 @@ if (config.use_env_variable) {
     sequelize = new Sequelize(process.env.DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
         host: process.env.DB_HOST,
         dialect: process.env.DB_DIALECT,
-        operatorsAliases: false,
+        operatorsAliases: 0,
         pool: {
             max: parseInt(process.env.POOL_MAX),
             min: parseInt(process.env.POOL_MIN),
